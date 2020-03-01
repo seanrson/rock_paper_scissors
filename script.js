adb66570f5aef815ac63eefeb66000f41f127151
seanrson <seanrichardson@outlook.com>
2020-02-29 18:52:08 -0800

    DOM version

--- script.js
+++ script.js
@@ -3,22 +3,26 @@ function randomInt(min, max)
 {
     return Math.round(Math.random() * (max - min) + min);
 }
-// generate a random computer selected move
+
+// generate a random computer move
 function computerPlay()
 {
     let randInt = randomInt(1, 3); // generate a random integer between 1 and 3
-    switch (randInt)
+    switch (randInt) // select corresponding move
     {
         case 1: return "rock"; break;
         case 2: return "paper"; break;
         case 3: return "scissors"; break;
     }
 }
+
 // play a round
-function playRound(playerSelection, computerSelection, scores)
+function playRound(playerSelection, computerSelection)
 {
     playerSelection = playerSelection.toLowerCase();
-    let msg = "TBD";
+    console.log(playerSelection);
+    console.log(computerSelection);
+    let msg = "This will display if no valid move selected"; // error check
     switch (playerSelection)
     {
         case "rock": // when the player chooses rock
@@ -32,13 +36,13 @@ function playRound(playerSelection, computerSelection, scores)
                 }
                 case "paper":
                 {
-                    scores[1]++;
+                    compScore++;
                     msg = "You lose! Paper beats rock";
                     break;
                 }
                 case "scissors":
                 {
-                    scores[0]++;
+                    playerScore++;
                     msg = "You win! Rock beats scissors";
                     break;
                 }
@@ -51,7 +55,7 @@ function playRound(playerSelection, computerSelection, scores)
             {
                 case "rock":
                 {
-                    scores[0]++; 
+                    playerScore++;
                     msg = "You win! Paper beats rock";
                     break;
                 }
@@ -62,7 +66,7 @@ function playRound(playerSelection, computerSelection, scores)
                 }
                 case "scissors":
                 {
-                    scores[1]++;
+                    compScore++;
                     msg = "You lose! Scissors beats paper";
                     break;
                 }
@@ -75,13 +79,13 @@ function playRound(playerSelection, computerSelection, scores)
             {
                 case "rock": 
                 {
-                    scores[1]++;
+                    compScore++;
                     msg = "You lose! Rock beats scissors";
                     break;
                 }
                 case "paper":
                 {
-                    scores[0]++;
+                    playerScore++;
                     msg = "You win! Scissors beats paper";
                     break;
                 }
@@ -94,22 +98,44 @@ function playRound(playerSelection, computerSelection, scores)
             break;
         }
     }
-    console.log(msg)
+    alert(msg);
 }
-// play a game
-function game()
+
+// select next player move from button click
+function nextMove(e)
 {
-    let scores = [0, 0]; // [playerScore, compScore]
-    for (i=0; i<5; i++)
-    {
-        let playerMove = prompt("Rock, paper, or scissors?")
-        let computerMove = computerPlay();
-        playRound(playerMove, computerMove, scores);
-    }
-    if (scores[0] > scores[1])
-        return "Congratulations, you won!"
-    else if (scores[0] < scores[1])
-        return "Sorry, you lost!"
-    else
-        return "It's a draw, since we played 5 rounds including ties!"
+    if (playerScore == 5 || compScore == 5) return; // stop playing once someone hits 5 points
+    console.log(e.target.textContent);
+    let playerSelection = e.target.textContent; // get playerSelection from button click
+    console.log(playerSelection);
+    let computerSelection = computerPlay(); // randomize computerSelection
+    console.log(computerSelection);
+
+    playRound(playerSelection, computerSelection); // play a round
+
+    console.log(playerScore);
+    console.log(compScore);
+    scoreboard.textContent = "playerScore: " + playerScore + "\ncompScore: " + compScore; // update scores
+
+    if (playerScore == 5) // check for winner
+        final.textContent = "Congratulations, you won!";
+    else if (compScore == 5) // check for winner
+        final.textContent = "Sorry, you lost!";
+    results.appendChild(final);
 }
+
+let playerScore = 0;
+let compScore = 0;
+
+const results = document.querySelector("#results")
+const scoreboard = document.createElement('h3');
+scoreboard.textContent = "playerScore: " + playerScore + "\ncompScore: " + compScore; // initialize scores
+results.appendChild(scoreboard);
+const final = document.createElement('h3');
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const moves = Array.from(document.querySelectorAll(".move"));
+
+moves.forEach(move => move.addEventListener('click', nextMove)); // play game upon button click
